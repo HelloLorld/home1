6a4c8f14c4db6694945c18bd6ef5847e83f43d62
Danilov Ruslan <62810505+HelloLorld@users.noreply.github.com>
2021-10-21 17:02:21 +0300

    Update MyPolynomial.java

--- src/com/company/other/MyPolynomial.java
+++ src/com/company/other/MyPolynomial.java
@@ -52,16 +52,13 @@ public class MyPolynomial {
         if (ourCoeffs.length>=rigthCoeffs.length) return new MyPolynomial(ourCoeffs);
         else return new MyPolynomial(rigthCoeffs);
     }
-    public MyPolynomial multiply(MyPolynomial right) {//Доделать
+    public MyPolynomial multiply(MyPolynomial right) {
         double[] newCoeffs;
         newCoeffs= new double[this.getDegree()+right.getDegree()+1];
         for (int i = this.coeffs.length-1;i>=0;i--)
             for(int j = right.coeffs.length-1;j>=0;j--)
             {
-                if (i==0) newCoeffs[j]+=coeffs[i]*right.coeffs[j];
-                else if(j==0) newCoeffs[i]+=coeffs[i]*right.coeffs[j];
-                else if(i==1||j==1) newCoeffs[i*j+1] += coeffs[i]*right.coeffs[j];
-                else newCoeffs[i+j] += coeffs[i]*right.coeffs[j];
+                newCoeffs[i+j] += coeffs[i]*right.coeffs[j];
             }
         return new MyPolynomial(newCoeffs);
     }
