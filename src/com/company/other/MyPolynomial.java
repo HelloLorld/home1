f8cd30f7b06f8ff16247c6a7066cccc002f5f578
Danilov Ruslan <62810505+HelloLorld@users.noreply.github.com>
2021-10-21 16:15:12 +0300

    Update MyPolynomial.java

--- src/com/company/other/MyPolynomial.java
+++ src/com/company/other/MyPolynomial.java
@@ -54,16 +54,15 @@ public class MyPolynomial {
     }
     public MyPolynomial multiply(MyPolynomial right) {//Доделать
         double[] newCoeffs;
-        if (this.getDegree()==1||right.getDegree()==1) newCoeffs = new double[this.getDegree()*right.getDegree()+2];
-        else newCoeffs= new double[this.getDegree()*right.getDegree()+1];
+        if (this.getDegree()==1||right.getDegree()==1) newCoeffs = new double[this.getDegree()+right.getDegree()+2];
+        else newCoeffs= new double[this.getDegree()+right.getDegree()+1];
         for (int i = this.coeffs.length-1;i>=0;i--)
             for(int j = right.coeffs.length-1;j>=0;j--)
             {
                 if (i==0) newCoeffs[j]+=coeffs[i]*right.coeffs[j];
                 else if(j==0) newCoeffs[i]+=coeffs[i]*right.coeffs[j];
                 else if(i==1||j==1) newCoeffs[i*j+1] += coeffs[i]*right.coeffs[j];
-                else newCoeffs[i*j] += coeffs[i]*right.coeffs[j];
-            }
+                else newCoeffs[i+j] += coeffs[i]*right.coeffs[j];
         return new MyPolynomial(newCoeffs);
     }
 }
